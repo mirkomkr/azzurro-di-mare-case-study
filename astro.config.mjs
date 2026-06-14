7f83a656ecd652906ab658331d713c273ba1a342
Mirko Passeri <bionikle746@gmal.com>
2026-06-14 07:57:35 +0200

    fix: update production domain in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // Placeholder: replace with the definitive production domain when available.
-  site: 'https://azzurro-di-mare-case-study.vercel.app',
+  site: 'https://azzurro-di-mare.mirkopasseri.it',
   integrations: [vue(), sitemap()],
 });
